bdc0c5e1014c67900d8ca5d16e63d29c908232e0
joshua40 <josh@knockmedia.com>
2018-07-11 15:43:56 -0400

    Added Read pts files
    Added creating dictionary and reading the pts files

--- person-identify.py
+++ person-identify.py
@@ -9,10 +9,69 @@
 #  Distributed under the terms of the GNU GENERAL PUBLIC LICENSE, 
 #  Version 3, 29 June 2007
 #-----------------------------------------------------------------------------
+
+import os 
+import itertools
+
 '''
     Load Data
 '''
-# Import points
+
+# Create Database
+def ImportPoints():
+    # Create person 22pts database
+    database = {}
+    #face_data = []
+
+    # note Create loop to import all data from database
+    
+    # Read files and import data into dictionary
+    file_name_ct = 1
+    fine_name_ftr = 1
+
+    # add men
+    file_name_gdr = 'm'
+    while file_name_ct < 77:
+        file_data = file_name_gdr+'-'+str('{0:03}'.format(file_name_ct))+'-'+str('{0:02}'.format(fine_name_ftr))+'.pts'
+        try:
+            with open(os.path.join('points_22/'+file_name_gdr+'-'+str('{0:03}'.format(file_name_ct)), file_data), 'r') as file_object:
+                data = file_object.read().split()
+                face_data = []
+                for element in data:
+                    try:
+                        face_data.append(float(element))
+                    except ValueError:
+                        pass
+        except IOError as e:
+            print('File ' + file_data + ' was not found')
+            pass
+
+        database[file_name_gdr+'-'+str('{0:03}'.format(file_name_ct))] = face_data[2:] 
+        file_object.close()
+        file_name_ct += 1
+
+    # add women
+    file_name_gdr = 'w'
+    while file_name_ct < 61:
+        file_data = file_name_gdr+'-'+str('{0:03}'.format(file_name_ct))+'-'+str('{0:02}'.format(fine_name_ftr))+'.pts'
+        try:
+            with open(os.path.join('points_22/'+file_name_gdr+'-'+str('{0:03}'.format(file_name_ct)), file_data), 'r') as file_object:
+                data = file_object.read().split()
+                face_data = []
+                for element in data:
+                    try:
+                        face_data.append(float(element))
+                    except ValueError:
+                        pass
+        except IOError as e:
+            print('File ' + file_data + ' was not found')
+            pass
+
+        database[file_name_gdr+'-'+str('{0:03}'.format(file_name_ct))] = face_data[2:] 
+        file_object.close()
+        file_name_ct += 1
+
+    return database
 
 '''
     Define Features
@@ -35,7 +94,9 @@
 '''
 def main():
     try:
-        print("hello")
+        print("Program has begun")
+        #testing creating the database
+        print('Database: ', ImportPoints())
 
     except IOError:
         print("The file was not found. Check name or path.")
